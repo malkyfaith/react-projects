1dcd357040c9b3fb8b3bad3231c22af6ef80a2f0
Malkeet Singh <malkeet@Malkeets-MacBook-Pro.local>
2018-09-24 11:22:07 +1000

    added webpack config

---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,23 @@
+// entry - output
+const path = require('path');
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 8080
+    }   
+}
