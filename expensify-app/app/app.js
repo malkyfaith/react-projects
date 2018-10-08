888de622167e4c1f6ac9f36f976e70f6a13a0a98
malkes <malkyhaina@gmail.com>
2018-10-08 23:01:50 +1100

    boilerplate

--- expensify-app/app/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/style.scss';
-
-ReactDOM.render(<p>This is a boilerplate</p>, document.getElementById('app'));
-
--- indecision-app/app/utils.js
+++
@@ -1,7 +0,0 @@
-console.log('utils.js is running.....');
-
-export const add = (a,b) => a+b;
-
-export const sub = (a,b) => a-b;
-
-export default 42;
