0db97cc69ee352a9d8d545a0ec3f9c041e7a8b52
malkes <malkyhaina@gmail.com>
2018-10-08 23:18:22 +1100

    initial commit - expensify app

---
+++ expensify-app/app/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+ReactDOM.render(<p>This is a expensify </p>, document.getElementById('app'));
+
